779d5ee09c971e2734a48cca92df69fc3e3f069b
erik <tuananh1421999@gmail.com>
2021-04-29 10:51:02 +0700

    stack6

---
+++ buffer_overflow/stack6/stack6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <signal.h>
+
+void segv_handler(int signal){
+    printf("you still lose !\n");
+    about();
+}
+
+void init(){
+    signal(SIGSEGV, segv_handler);
+}
+
+int main(){
+    int cookie;
+    char buf[16];
+    init();
+    printf("&buf: %p, &cookie: %p\n", buf, &cookie);
+    gets(buf);
+    if(cookie == 0x000D0A00){
+        printf("You lose!\n");
+    }
+}
